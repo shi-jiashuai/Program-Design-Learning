b9d7d3fba9a0a6c5e887bca915189d5166a97f75
Topologies <shi_jiashuai@hotmail.com>
2020-09-06 14:34:54 +0800

    数组实现

--- Code-Practice/MyString/main.cpp
+++ Code-Practice/MyString/main.cpp
@@ -1,76 +1,27 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<iostream>
+#include <numeric>
+#include <iostream>
+#include <vector>
+//#include <algorithom>
 using namespace std;
-//#include <string>
-#include "myString.h"
+#include <numeric>
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> returnVector;
+        vector <int>::iterator Iter1;
+        int j = 0, partotal;
 
+        for (Iter1 = nums.begin() + 1; Iter1 != nums.end() + 1; Iter1++)
+        {
+            partotal = accumulate(nums.begin(), Iter1, 0);
+            returnVector[j] = partotal;
+            j++;
+        }
+        return returnVector;
+    }
+};
 
-void test01()
+int main()
 {
-    MyString str = "abc";
-
-    cout << str << endl;
-
-    cout << "请重新给str赋值：" << endl;
-
-    cin >> str;
-
-    cout << "str 新的值为： " << str << endl;
-
-
-    MyString str2 = str;
-
-    cout << "str2 = " << str2 << endl;
-
-}
-
-void test02()
-{
-    MyString str = "abcd";
-
-    MyString str2 = "aaa";
-
-    str2 = str;
-
-    cout << "str2 = " << str2 << endl;
-
-    cout << "str2[0] = " << str2[0] << endl;
-
-    str2[0] = 'z';
-
-    cout << "str2[0]改为z后输出：  " << str2 << endl;
-
-    MyString str3 = "abc";
-    MyString str4 = "def";
-    MyString str5 = str3 + str4;
-    MyString str6 = str5 + "ghe";
-    cout << "str5 = " << str5 << endl;
-    cout << "str6 = " << str6 << endl;
-
-    if (str5 == str6)
-    {
-        cout << "str5 == str6" << endl;
-    }
-    else
-    {
-        cout << "str5 != str6" << endl;
-    }
-
-    if (str6 == "abcdefghe")
-    {
-        cout << "str6 = abcdefghe" << endl;
-    }
-    else
-    {
-        cout << "str6 != abcdefghe" << endl;
-    }
-}
-
-
-int main() {
-    test01();
-    test02();
-
-    system("pause");
-    return EXIT_SUCCESS;
+    return 0;
 }
