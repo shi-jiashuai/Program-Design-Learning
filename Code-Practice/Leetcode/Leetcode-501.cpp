8dc871ad14882384bb124ecabfa74c1444300fee
Topologies <shi_jiashuai@hotmail.com>
2020-09-24 19:13:47 +0800

    二叉树-Morris 中序遍历

---
+++ Code-Practice/Leetcode/Leetcode-501.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int base, count, maxCount;
+    vector<int> answer;
+
+    void update(int x) {
+        if (x == base) {
+            ++count;
+        } else {
+            count = 1;
+            base = x;
+        }
+        if (count == maxCount) {
+            answer.push_back(base);
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            answer = vector<int> {base};
+        }
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        TreeNode *cur = root, *pre = nullptr;
+        while (cur) {
+            if (!cur->left) {
+                update(cur->val);
+                cur = cur->right;
+                continue;
+            }
+            pre = cur->left;
+            while (pre->right && pre->right != cur) {
+                pre = pre->right;
+            }
+            if (!pre->right) {
+                pre->right = cur;
+                cur = cur->left;
+            } else {
+                pre->right = nullptr;
+                update(cur->val);
+                cur = cur->right;
+            }
+        }
+        return answer;
+    }
+};
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/find-mode-in-binary-search-tree/solution/er-cha-sou-suo-shu-zhong-de-zhong-shu-by-leetcode-/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
